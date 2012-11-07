0620a5cd38da7e4e9b9b6656e8c4d06c2042cf42
Matthew O'Brien <ChaosPandion@gmail.com>
2012-11-07 16:06:14 -0500

    Saving Work

--- BoldAspect.Test/Program.cs
+++ BoldAspect.Test/Program.cs
@@ -21,93 +21,93 @@ namespace BoldAspect.Test
             
         }
 
-        static void EX2()
-        {
-            var sb = new StringBuilder();
-            var dir = new DirectoryInfo("C:/Users/Matthew/Projects/BoldAspect/BoldAspect/CLI/Metadata/IndexTags");
-            var files = dir.GetFiles();
-            foreach (var file in files)
-            {
-                var fn = "BoldAspect.CLI.Metadata." + file.Name.Replace(".cs", "").Replace("Index", "");
-                var type = Assembly.GetAssembly(typeof(PortableExecutable)).GetType(fn);
-                sb.AppendFormat("new CodedIndex(typeof({0}), new [] {{ ", type.Name);
-                var ts = Enum.GetNames(type);
-                var first = true;
-                foreach (var t in ts.Where(tn => !tn.StartsWith("Not")))
-                {
-                    if (!first)
-                    {
-                        sb.Append(", ");
-                    }
-                    sb.AppendFormat("TableID.{0}", t);
-                    first = false;
-                }
-                sb.Append("}),\r\n");
-            }
-            Debug.Write(sb.ToString());
-        }
+        //static void EX2()
+        //{
+        //    var sb = new StringBuilder();
+        //    var dir = new DirectoryInfo("C:/Users/Matthew/Projects/BoldAspect/BoldAspect/CLI/Metadata/IndexTags");
+        //    var files = dir.GetFiles();
+        //    foreach (var file in files)
+        //    {
+        //        var fn = "BoldAspect.CLI.Metadata." + file.Name.Replace(".cs", "").Replace("Index", "");
+        //        var type = Assembly.GetAssembly(typeof(PortableExecutable)).GetType(fn);
+        //        sb.AppendFormat("new CodedIndex(typeof({0}), new [] {{ ", type.Name);
+        //        var ts = Enum.GetNames(type);
+        //        var first = true;
+        //        foreach (var t in ts.Where(tn => !tn.StartsWith("Not")))
+        //        {
+        //            if (!first)
+        //            {
+        //                sb.Append(", ");
+        //            }
+        //            sb.AppendFormat("TableID.{0}", t);
+        //            first = false;
+        //        }
+        //        sb.Append("}),\r\n");
+        //    }
+        //    Debug.Write(sb.ToString());
+        //}
 
 
-        static void EX1()
-        {
-            var sb = new StringBuilder();
-            foreach (var n in Enum.GetNames(typeof(TableID)))
-            {
-                sb.AppendFormat("new SimpleIndex(TableID.{0}),\r\n", n);
-            }
-            Debug.Write(sb.ToString());
-        }
-        static void EX()
-        {
-            var i = 0;
-            var sb = new StringBuilder();
-            foreach (var n in Enum.GetNames(typeof(TableID)))
-            {
-                sb.AppendFormat("new TableSchema(TableID.{0},\r\n", n);
-                var t = Assembly.GetAssembly(typeof(PortableExecutable)).GetType("BoldAspect.CLI.Metadata." + n + "Record");
-                var fs = t.GetFields();
-                var first = true;
-                foreach (var f in fs)
-                {
-                    if (!first)
-                    {
-                        sb.Append(",\r\n");
-                    }
-                    sb.Append("        ");
-                    var ia = Attribute.GetCustomAttribute(f, typeof(ColumnAttribute), true);
-                    var cc = ia as ConstantColumnAttribute;
-                    if (cc != null)
-                    {
-                        sb.AppendFormat("typeof({0})", cc.Type.Name);
-                    }
-                    var si = ia as SimpleIndexAttribute;
-                    if (ia is SimpleIndexAttribute)
-                    {
-                        sb.AppendFormat("SimpleIndexes[{0}]", Enum.GetNames(typeof(TableID)).ToList().IndexOf(si.TableID.ToString()));
-                    }
-                    var ci = ia as CodedIndexAttribute;
-                    if (ci != null)
-                    {
-                        sb.AppendFormat("CodedIndexes.Single(c => c.EnumType == typeof({0}))", ci.EnumType.Name);
-                    }
-                    if (ia is StringHeapIndexAttribute)
-                    {
-                        sb.Append("typeof(StringsHeapIndex)");
-                    }
-                    if (ia is GuidHeapIndexAttribute)
-                    {
-                        sb.Append("typeof(GuidHeapIndex)");
-                    }
-                    if (ia is BlobHeapIndexAttribute)
-                    {
-                        sb.Append("typeof(BlobHeapIndex)");
-                    }
-                    first = false;
-                }
-                sb.Append("),\r\n");
-            }
-            Debug.Write(sb.ToString());
-        }
+        //static void EX1()
+        //{
+        //    var sb = new StringBuilder();
+        //    foreach (var n in Enum.GetNames(typeof(TableID)))
+        //    {
+        //        sb.AppendFormat("new SimpleIndex(TableID.{0}),\r\n", n);
+        //    }
+        //    Debug.Write(sb.ToString());
+        //}
+        //static void EX()
+        //{
+        //    var i = 0;
+        //    var sb = new StringBuilder();
+        //    foreach (var n in Enum.GetNames(typeof(TableID)))
+        //    {
+        //        sb.AppendFormat("new TableSchema(TableID.{0},\r\n", n);
+        //        var t = Assembly.GetAssembly(typeof(PortableExecutable)).GetType("BoldAspect.CLI.Metadata." + n + "Record");
+        //        var fs = t.GetFields();
+        //        var first = true;
+        //        foreach (var f in fs)
+        //        {
+        //            if (!first)
+        //            {
+        //                sb.Append(",\r\n");
+        //            }
+        //            sb.Append("        ");
+        //            var ia = Attribute.GetCustomAttribute(f, typeof(ColumnAttribute), true);
+        //            var cc = ia as ConstantColumnAttribute;
+        //            if (cc != null)
+        //            {
+        //                sb.AppendFormat("typeof({0})", cc.Type.Name);
+        //            }
+        //            var si = ia as SimpleIndexAttribute;
+        //            if (ia is SimpleIndexAttribute)
+        //            {
+        //                sb.AppendFormat("SimpleIndexes[{0}]", Enum.GetNames(typeof(TableID)).ToList().IndexOf(si.TableID.ToString()));
+        //            }
+        //            var ci = ia as CodedIndexAttribute;
+        //            if (ci != null)
+        //            {
+        //                sb.AppendFormat("CodedIndexes.Single(c => c.EnumType == typeof({0}))", ci.EnumType.Name);
+        //            }
+        //            if (ia is StringHeapIndexAttribute)
+        //            {
+        //                sb.Append("typeof(StringsHeapIndex)");
+        //            }
+        //            if (ia is GuidHeapIndexAttribute)
+        //            {
+        //                sb.Append("typeof(GuidHeapIndex)");
+        //            }
+        //            if (ia is BlobHeapIndexAttribute)
+        //            {
+        //                sb.Append("typeof(BlobHeapIndex)");
+        //            }
+        //            first = false;
+        //        }
+        //        sb.Append("),\r\n");
+        //    }
+        //    Debug.Write(sb.ToString());
+        //}
 
     }
 }
---
+++ BoldAspect/CLI/Metadata/MetadataReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoldAspect.CLI.Metadata.MetadataStreams;
+
+namespace BoldAspect.CLI.Metadata
+{
+    public sealed class MetadataReader
+    {
+        private readonly MetadataRoot _root;
+
+        public MetadataReader(MetadataRoot root)
+        {
+            _root = root;
+        }
+
+
+    }
+}
